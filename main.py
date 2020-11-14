16a0a9391d94cbe450e41bfbb0b660fc830bc295
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-11-14 12:17:36 +0530

    for loop basic

--- main.py
+++ main.py
@@ -115,3 +115,83 @@
 # Student 2
 # Student 1
 # Student 3
+
+# List Data Type 
+
+#          -5  -4  -3   -2       -1   
+#           0   1   2   3         4  <- index
+# numbers = [10, 20, 30, 12.123, "asdasd"]
+
+# numbers.append(-20) 
+# numbers.remove(10)
+# del numbers[2]
+
+# print( len(numbers) )
+
+# print( numbers[1:4] )
+# print( numbers )
+
+# numbers = numbers[1:4]
+
+# print(list("Hello"))
+
+# numbers = []
+# numbers = list()
+# print(list())
+
+
+# Tuples
+
+# numbers = [10]
+# # print( len(numbers) )
+# # numbers.add("asdasd")
+# numbers[0] = 20
+# print(numbers[0])
+# tuple()
+
+# text = "Hello"
+# print(text[1:3])
+# text = text + " Python"
+
+
+# Range
+# range(end), 0 --- end - 1
+# range(start, end),  start --- end - 1
+# range(start, end, step), start start+step, start + 2xstep, .... end - 1
+# numbers = range(2, 51, 2)
+# print(list(numbers))
+
+# range(4, 0, -1) 4, 3, 2, 1
+
+# numbers = [1, 2, 3, 4]
+# numbers.reverse()
+
+# print(numbers)
+
+# print( list(reversed(numbers)) )
+# print(numbers)
+
+# Membership Operators
+#    in, not in
+
+# Loops
+
+# seq = [20, -10, 40]
+
+# # For Loop
+# for i in range(1, 20):
+#     print(i)
+#     if (i > 0):
+#         print(i)
+
+# print("Finish")
+
+# scores = []
+
+# for i in range(1, 6):
+#     s = int(input("Student " + str(i) + " Score: "))
+#     scores.append(s)
+
+# print(scores)
+
+# Exercise : Show the max value (5 inputs)
